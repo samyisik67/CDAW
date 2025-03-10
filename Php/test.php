fe0abe7a023a4ec968e98fe377c5baf6da0e636a
samyisik67 <samy.isik@etu.imt-nord-europe.fr>
2025-03-10 11:53:01 +0100

    Intro php

--- Php/test.php
+++ Php/test.php
@@ -1,1 +1,25 @@
-test
+<?php 
+
+?>
+<html>
+    <head>
+        <title> Cours PHP et MySQL</title>
+        <meta charset="utf-8">
+    </head>
+    <body>
+        <h1>Titre principal</h1>
+        <?php
+            //Affiche "Heelo World" avec un retour à la ligne
+            echo "<h2>Première instruciton PHP avec echo</h2>";
+            echo 'Hello World <br>';
+            echo 29;
+            echo "<br>J'ai" ,29,"ans <br>";
+
+            print "<h2>Hello World </h2>";
+            print "Bonjour à tous ! <br>";
+            print 29;
+            
+        ?>
+        <p>Un paragraphe</p>
+    </body>
+</html>
