e1021fa298dd025c4115627da40d3b75b672d13b
samyisik67 <samy.isik@etu.imt-nord-europe.fr>
2025-03-11 10:54:38 +0100

    TP PDO ajout à la base de données

--- BackEnd/tp1/tes-PDO-post.php
+++ BackEnd/tp1/tes-PDO-post.php
@@ -17,7 +17,7 @@
         while(!empty($data)){
         echo '<tr> 
                 <li>
-                <td>'  .$data->id. '</td>
+                <td>' .$data->id. '</td>
                 <td>' .$data->name.'</td>
                 <td>'.$data->email.'</td>
                 </li>
@@ -26,6 +26,26 @@
         }
     '</table>';        
     
+    echo '<form method="POST" action="tes-PDO-post.php">';
+    echo '<label for="name">Votre Nom :</label>';
+    echo '<input type="text" id="name" name="name" required>';
+    echo '<br>';
+    echo '<label for="email">Votre email :</label>';
+    echo '<input type="text" id="email" name="email" required>';
+    echo '<br>';
+    echo '<input type="submit" "name=ok" value="ok">';
+    echo '<br>';
+    echo '</form>';
+    
+    if($_SERVER["REQUEST_METHOD"] == "POST"){
+        $name = htmlspecialchars($_POST['name']);
+        $email = htmlspecialchars($_POST['email']);
+
+        $stmt = $pdo->prepare("INSERT INTO users  VALUES (0, :name, :email)");
+        $stmt->bindParam(':name', $name);
+        $stmt->bindParam(':email', $email);
+        $stmt->execute();
+    };  
     /*** close the database connection ***/
     $pdo = null;
 ?>
